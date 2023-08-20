809e905307ef2a6208b10049ea00b8760ce6bd3a
Farhad Dalirani <dalirani.1373@gmail.com>
2023-08-20 01:04:25 -0400

    update readme

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# siamese_net_stereo_depth
-A straightforward Siamese network designed for block matching to generate a disparity map
+# Siamese CNN for Block Matching in Stereo Images Depth Mapping
+
+Stereo image depth mapping often involves rectifying images and then establishing corresponding points between the left and right images. Patch matching is a common method for finding these corresponding points, where a neighborhood of pixels in both images is compared. Many computer vision libraries offer simple methods like Sum of Absolute Differences (SAD) for comparing patch similarity.
+
+This repository contains a Siamese Convolutional Neural Network trained using Hinge loss. The network is trained on a subset of the KITTI 2015 disparity dataset.
+
+The following figures illustrate the Siamese network architecture and its comparison with the OpenCV block matching algorithm. As depicted, the Siamese network produces higher-quality disparity images.
+
+<p align="center">
+  <img src="readme-image/Disparity-Siamese-opencv.gif" alt="Siamese vs OpenCV" width="900">
+</p> 
+<p align="center">
+  <img src="readme-image/model-arch.png" alt="Model Architecture" width="900">
+</p> 
+
+To experiment with more complex neural networks, modify the PyTorch model in `model.py`.
+For more intricate models, you can augment the `KITTI_disparity_subset` dataset with more sampe of KITTI.
+To generate disparity maps, utilize the functions in `disparity_map.py`, and refer to the example in `example_disparity_KITTI_siamese.py`.
--- example_disparity_KITTI_siamese.py
+++ example_disparity_KITTI_siamese.py
@@ -3,6 +3,7 @@ from model import SiameseNetStereoMatching
 from stereo_batch_provider import KITTIDisparityDataset
 import matplotlib.pyplot as plt
 from disparity_map import disparity_map_nn
+import cv2
 
 
 if __name__ == '__main__':
@@ -25,21 +26,43 @@ if __name__ == '__main__':
     for sample_i in range(0, 5):
         img_l, img_r = dataset_test[sample_i]
 
-        # obtain disparity map
-        disparity_map = disparity_map_nn(model=model, img_l=img_l, img_r=img_r, padding_size=padding_size, max_disparity=50, device=device)
-        disparity_map = disparity_map.cpu().numpy()
+        # obtain disparity map by Siamses matching
+        disparity_map_siamese = disparity_map_nn(model=model, img_l=img_l, img_r=img_r, padding_size=padding_size, max_disparity=64, device=device)
+        disparity_map_siamese = disparity_map_siamese.cpu().numpy()
 
-        plt.figure(figsize=(10, 5))
+        # Convert the float images to 8-bit format (0-255)
+        left_image = cv2.normalize(img_l, None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8U)
+        right_image = cv2.normalize(img_r, None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8U)
+
+        # obtain disparity map by OpencvStereo matching
+        # Create a StereoBM object
+        stereo_bm = cv2.StereoBM_create(numDisparities=64, blockSize=5)
+        # Compute the disparity map
+        disparity_map_5_opencv = stereo_bm.compute(left_image, right_image)
+        # Create a StereoBM object
+        stereo_bm = cv2.StereoBM_create(numDisparities=64, blockSize=9)
+        # Compute the disparity map
+        disparity_map_9_opencv = stereo_bm.compute(left_image, right_image)
+
+        plt.figure(figsize=(12, 5))
         # Create the first subplot for img_l
-        plt.subplot(2, 1, 1)
+        plt.subplot(2, 2, 1)
         plt.imshow(img_l.squeeze(), cmap='gray')
         plt.title('Left Image')
         plt.axis('off')  
         # Create the second subplot for disparity_map
-        plt.subplot(2, 1, 2)
-        plt.imshow(disparity_map)
+        plt.subplot(2, 2, 2)
+        plt.imshow(disparity_map_siamese)
         plt.title('Disparity Map (Siamese CNN)')
         plt.axis('off')  
+        plt.subplot(2, 2, 3)
+        plt.imshow(disparity_map_5_opencv)
+        plt.title('Disparity Map (OpenCV StereoBM_create), block size 5')
+        plt.axis('off')  
+        plt.subplot(2, 2, 4)
+        plt.imshow(disparity_map_9_opencv)
+        plt.title('Disparity Map (OpenCV StereoBM_create), block size 9')
+        plt.axis('off')  
         plt.tight_layout()
-    
+        plt.savefig('./readme-image/Disparity-Siamse-opencv-{}.png'.format(sample_i))
     plt.show()
